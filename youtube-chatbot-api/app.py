426586374f9604b68694aaae9657d3c160a0e26e
GraphicalGiant <namansahu222002@gmail.com>
2025-08-06 17:32:17 +0530

    added cors

--- youtube-chatbot-api/app.py
+++ youtube-chatbot-api/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
 from chatbot_module import generate_answer  # Your RAG code
 
+from flask_cors import CORS
+
+
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/chat", methods=["POST"])
 def chat():
